e804ace01b8836b1f7c63682e8771feef95eee9b
Ana Ercegovac <ana.ercegovac09@gmail.com>
2021-05-24 14:08:51 +0200

    Added parsing arguments

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,20 @@
 
 int main(int argc, char *argv[])
 {
-    Assembly as("tests/zad9.s", "tests/zad9.o");
+    if (argc != 4) {
+        std::cout << "Error: Not enough arguments" << std::endl;
+        return -4;
+    }
+    if (strcmp(argv[1], "-o")) {
+        std::cout << "Option: " << argv[1] << " not defined" << std::endl;
+        return -4;
+    }
+
+    std::string inputFilename(argv[3]);
+    std::string outputFilename(argv[2]);
+
+    Assembly as("tests/" + inputFilename, "tests/" + outputFilename);
+
     if (!as.assemble()) 
         std::cout << "Success" << std::endl;
     
